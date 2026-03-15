6c77b55307731c6e8ab669ebed80bf3f8e456ced
notnico22 <notnicotv@gmail.com>
2026-03-15 12:50:07 -0400

    Core File Updates

--- BetterCrewLink/BetterCrewLinkPlugin.cs
+++ BetterCrewLink/BetterCrewLinkPlugin.cs
@@ -3,28 +3,22 @@ using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using MiraAPI.PluginLoading;
 using Reactor;
-using Reactor.Networking;
-using Reactor.Networking.Attributes;
-using Rewired.Utils.Classes.Data;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics.CodeAnalysis;
 
 namespace BetterCrewLink;
 
-[BepInAutoPlugin("com.goober.bettercrewlink", "BetterCrewLink")]
+[BepInAutoPlugin("com.naum.bettercrewlink", "BetterCrewLink")]
 [BepInProcess("Among Us.exe")]
 [BepInDependency(ReactorPlugin.Id)]
-[ReactorModFlags(ModFlags.None)]
-public partial class BetterCrewLinkPlugin : BasePlugin
+[BepInDependency("mira.api")]
+public partial class BetterCrewLinkPlugin : BasePlugin, IMiraPlugin
 {
     public const string VersionString = "1.0.0";
     [SuppressMessage("Style", "S1104", Justification = "Required by BepInEx plugin metadata")]
     public static readonly Version PluginVersion = System.Version.Parse(VersionString);
     public Harmony Harmony { get; } = new(Id);
+    public string OptionsTitleText => "BetterCrewLink";
 
     /////////////////////
     public const bool IsDevRelease = true;
@@ -45,6 +39,7 @@ public partial class BetterCrewLinkPlugin : BasePlugin
         try
         {
             Harmony.PatchAll();
+            InitializeRuntime();
         }
         catch (Exception exception)
         {
@@ -56,4 +51,6 @@ public partial class BetterCrewLinkPlugin : BasePlugin
 
         BCLLogger.Debug("BetterCrewLink finished loading!");
     }
+
+    public BepInEx.Configuration.ConfigFile GetConfigFile() => Config;
 }
